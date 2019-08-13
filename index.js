ba6d1156c65f403a3d854dd71cb7cba877a95147
bhchen81 <bhchen81@gmail.com>
2019-08-14 00:28:18 +0800

    3.8 completed, display info of request body in morgan middleware

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", req => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
