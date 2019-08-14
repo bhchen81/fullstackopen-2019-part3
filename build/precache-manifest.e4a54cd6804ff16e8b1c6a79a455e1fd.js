f4493f1f679a95d5447670f0318dd9496354b660
bhchen81 <bhchen81@gmail.com>
2019-08-14 09:48:00 +0800

    Build UI and deploy to Heroku

---
+++ build/precache-manifest.e4a54cd6804ff16e8b1c6a79a455e1fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f14710558e36924997cf97930c2ef9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceeebeb64f04c539eb56",
+    "url": "/static/css/main.50ae8206.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "ceeebeb64f04c539eb56",
+    "url": "/static/js/main.feee821c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
